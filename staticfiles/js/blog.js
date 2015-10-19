c1a483a1d70d7ed9cd6b4392058c8b87f7fdcf27
Deathridge <danielrbetteridge@gmail.com>
2015-10-19 12:39:51 +0800

    created function to retrieve posts from api

--- staticfiles/js/blog.js
+++ staticfiles/js/blog.js
@@ -3,5 +3,18 @@ $( document ).ready(function() {
 	$('#flex-menu-3').click(function(){
 		window.location.href = "../templates/images.html";
 	});
+
+
+
+	$.ajax({
+		method: "GET",
+		url: "api.blogger.danielbetteridge.com/post",
+		success: function(data){
+			getPosts(data);
+		}
+	})
 });
 
+function getPosts(data){
+	alert(data);
+}

5332b753d770c2adf8bea1ee5c9fe3bbb62370c1
Deathridge <danielrbetteridge@gmail.com>
2015-10-19 12:51:02 +0800

    created function to retrieve posts from api

--- blogger/settings.py
+++ blogger/settings.py
@@ -58,6 +58,8 @@ CORS_ORIGIN_WHITELIST = (
         'blogger.danielbetteridge.com'
     )
 
+CORS_ORIGIN_ALLOW_ALL = True
+
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.

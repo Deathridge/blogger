b626ddbe693ea2981620d990beb04432834338d6
Deathridge <danielrbetteridge@gmail.com>
2015-10-20 12:40:54 +0800

    js changes to attempt async image loads

--- staticfiles/js/blog.js
+++ staticfiles/js/blog.js
@@ -41,7 +41,7 @@ $( document ).ready(function() {
 						
 						$("#" + $(this).attr("id")).attr("src", $(this).attr("src"));
 					});
-					$downloadingImage.attr("id", i + j);
+					$downloadingImage.attr("id", "" + i +"" + j + "");
 					$downloadingImage.attr("src", data.Image);
 				}
 			});

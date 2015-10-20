3fd2527ed9951f14ac704cda9206035eacb5f52e
Deathridge <danielrbetteridge@gmail.com>
2015-10-20 14:20:01 +0800

    js changes to attempt async image loads

--- staticfiles/js/blog.js
+++ staticfiles/js/blog.js
@@ -11,15 +11,13 @@ $( document ).ready(function() {
 		url: "http://api.blogger.danielbetteridge.com/posts?format=json",
 		success: function(data){
 			buildPosts(data);
+			addImages(data);
 		}
 	})
 
 	function buildPosts(data){
 	posts = data;
-	var i=0;
-	var j=0;
-	var image = new Array();
-	for (i=0;i<posts.length;i++){ 
+	for (var i=0;i<posts.length;i++){ 
 		var title = posts[i].Title;
 		var images = posts[i].Images;
 		var created = posts[i].created;
@@ -28,39 +26,38 @@ $( document ).ready(function() {
 		var url = posts[i].url;
 		
 		$('.content-container').append('<div class="flex-item flex-text"><h1>'+ title+ ' </h1><p>'+content+'</p></div>');
-		$('.content-container').append('<div class=image-container id='+i+'></div>')
-		for (j=0;j<images.length;j++){
+		$('.content-container').append('<div class=image-container id='+i+'></div>')	
+
+	}	
+	};
+
+	function addImages(data){
+		posts=data;
+	for (var i=0;i<posts.length;i++){ 
+
+		var title = posts[i].Title;
+		var images = posts[i].Images;
+		var created = posts[i].created;
+		var modified = posts[i].modified;
+		var content = posts[i].Content_Text;		
+		var url = posts[i].url;
+		for (var j=0;j<images.length;j++){
 			var id = "#" + i;
-			
-			
-			
-			$(id).append('<div class="flex-item"><img src="" id=' + i+ j +'></img></div>');
-			
+					
 			$.ajax({
 				method: "GET",
 				//async: false,
 				url: images[j],
 				success: function(data){
-					image["" +i+j] = data.Image;
-					updateImages(image, posts, images);				
-					console.log(data.Image);
+					$(id).append('<div class="flex-item"><img src="'+data.Image+'"></img></div>');
 				}
 			});
 			
 		}
-
 	}
-	
 	};
 
-	function updateImages(image, posts, images){
-		for (i=0;i<posts.length;i++){ 
-			for (j=0;j<images.length;j++){
-				$("#" + i + j).attr("src", image[""+i+j]);
-			}
-		}	
-	}
-
+	
 
 
 });
